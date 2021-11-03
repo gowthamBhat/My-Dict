47f586f123ede133526e071a4808253600be411f
Gowtham  H D <gowthambhat793@gmail.com>
2021-11-03 20:39:52 +0530

    calling api end point

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Container } from '@material-ui/core'
+import Header from './components/Header/Header'
 
 function App() {
+  useEffect(() => {
+    dictAPI()
+  }, [])
+  const [meanings, setMeanings] = useState([])
+
+  const dictAPI = async () => {
+    const { data } = await axios.get(
+      'https://api.dictionaryapi.dev/api/v2/entries/en/mother'
+    )
+    setMeanings(data)
+  }
+  console.log(meanings)
+
   return (
-    <div className="App">
-      <p>hello world</p>
+    <div
+      className="App"
+      style={{ backgroundColor: '#282c34', height: '100vh', color: 'white' }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+      >
+        <Header />
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
